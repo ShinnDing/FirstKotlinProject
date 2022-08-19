bdbb3a859ff599f69e18340f726afbd1e5ab2e57
shinn <stephanieshinn@outlook.com>
2022-08-19 12:02:53 -0500

    Pushing each time I make major changes, just to practice. Watching a YouTube tutorial for a crash course.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,11 @@
+fun getGreeting(): String = "Hello World! I'm Stephanie Shinn and this is my first time using Kotlin."
+
+fun sayHello() {
+    println(getGreeting())
+}
+
 fun main() {
-
-    //var is mutable and the values can be defined
-    var hello: String "Hello World!"
-
-    //val can only be assigned once
-    val name: String = "Stephanie Shinn"
-
-    //introduction to the world
-    println("Hello World! I'm Stephanie Shinn and this is my first time trying Kotlin.")
-
-
+    println("Hello World!")
+    println(getGreeting())
+    sayHello()
 }
