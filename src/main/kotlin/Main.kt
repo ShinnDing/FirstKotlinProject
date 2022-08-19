e74ff6f63babef2a95638a435ed862f5404ac973
shinn <stephanieshinn@outlook.com>
2022-08-19 11:37:32 -0500

    Added that I'm trying Kotlin for the first time. Also added var and val, but have not used them yet. Received warnings about the unused variables.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
-fun main(args: Array<String>) {
-    println("Hello World! I'm Stephanie Shinn.")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //var is mutable and the values can be defined
+    var hello: String "Hello World!"
+
+    //val can only be assigned once
+    val name: String = "Stephanie Shinn"
+
+    //introduction to the world
+    println("Hello World! I'm Stephanie Shinn and this is my first time trying Kotlin.")
+
+
 }
