6582505b4f2582229a6819c1f3cc8965e1613580
shinn <stephanieshinn@outlook.com>
2022-08-20 05:12:20 -0500

    Redoing Java threads project in Kotlin.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,7 @@
-fun getGreeting(): String = "Hello World! I'm Stephanie Shinn and this is my first time using Kotlin."
 
-fun sayHello() {
-    println(getGreeting())
-}
 
 fun main() {
-    println("Hello World!")
-    println(getGreeting())
-    sayHello()
+
+Threads().printTheMessage()
+
 }
